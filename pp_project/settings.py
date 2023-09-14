2acdd03177434d922d516625ea59c14e3a3ae021
LeonardoCapocci <leocap2001@gmail.com>
2023-09-14 13:39:21 -0700

    updated settings to include pizza_pizza

--- pp_project/settings.py
+++ pp_project/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # My apps.
+    'pizza_pizza',
+    # Default django apps.
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

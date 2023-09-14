332986d3680abbafb2cec2a7cea900a80825ec4f
LeonardoCapocci <leocap2001@gmail.com>
2023-09-14 14:12:32 -0700

    made a home page

--- pizza_pizza/views.py
+++ pizza_pizza/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    """The home page for Pizza Recipe List."""
+    return render(request, 'pizza_pizza/index.html')

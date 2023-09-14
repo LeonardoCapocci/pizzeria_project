f15ebaed2cd925dfb3ef30a80c950ed39065e8ee
LeonardoCapocci <leocap2001@gmail.com>
2023-09-14 12:44:05 -0700

    added pizza and toppings entry options

--- pizza_pizza/models.py
+++ pizza_pizza/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Pizza(models.Model):
+    """A pizza the user salivates about."""
+    name = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class Toppings(models.Model):
+    """The toppings that belong to the pizza."""
+    pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return a simple string representing the entry."""
+        if len(self.text) > 50:
+            return f"{self.text[:50]}..."
+        else:
+            return self.text

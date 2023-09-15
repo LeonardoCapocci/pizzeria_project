81c42a207c8e4202f0edfb6a16633e728d0d165b
LeonardoCapocci <leocap2001@gmail.com>
2023-09-15 12:06:21 -0700

    added pages for pizzas and their toppings

--- pizza_pizza/urls.py
+++ pizza_pizza/urls.py
@@ -4,8 +4,12 @@ from django.urls import path
 
 from . import views
 
-app_name = 'learning_logs'
+app_name = 'pizza_pizza'
 urlpatterns = [
     # Home page
     path('', views.index, name='index'),
+    # Pizzas page
+    path('pizzas/', views.pizzas, name='pizzas'),
+    # A page of a single pizza's toppings
+    path('pizzas/<int:pizza_id>/', views.toppings, name='pizza')
 ]
--- pizza_pizza/views.py
+++ pizza_pizza/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
+from .models import Pizza
 
 # Create your views here.
 def index(request):
     """The home page for Pizza Recipe List."""
     return render(request, 'pizza_pizza/index.html')
+
+def pizzas(request):
+    """The page that lists all tracked pizzas."""
+    pizzas = Pizza.objects.order_by('date_added')
+    context = {'pizzas':pizzas}
+    return render(request, 'pizza_pizza/pizzas.html', context)
+
+def toppings(request, pizza_id):
+    """The page that lists a single pizza's toppings."""
+    pizza = Pizza.objects.get(id=pizza_id)
+    toppings = pizza.toppings_set.order_by('-date_added')
+    context = {'pizza': pizza, 'toppings': toppings}
+    return render(request, 'pizza_pizza/pizza.html', context)
